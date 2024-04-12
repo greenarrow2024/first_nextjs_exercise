34919a08d32b4637edaa84897a364cd9baf7fe24
Green Arrow 2024 <haribon2301@gmail.com>
2024-04-12 17:21:41 +0800

    Added Rollur

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,8 +12,8 @@ function Hero() {
           className="object-contain"
         />
         <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
-          Explore The Green Arrow<span className="red-gradient">Diverse Realms</span> of
-          Anime Magic
+          Explore The<span className="red-gradient">Diverse Realms</span> of
+          Anime Magic - Rollur
         </h1>
       </div>
       <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
